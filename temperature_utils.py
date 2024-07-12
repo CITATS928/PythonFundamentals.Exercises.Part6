c3b6674fc799428b02c34805ab81134e59f662c3
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-11 20:28:02 -0400

    Finished

--- temperature_utils.py
+++ temperature_utils.py
@@ -1,3 +1,4 @@
+import math
 from typing import Iterable, Tuple
 
 
@@ -8,7 +9,7 @@ def convert_to_celsius(fahrenheit_temp: float) -> float:
     :param fahrenheit_temp: A float representing a temperature in fahrenheit
     :return: A float representing the corresponding value of the fahrenheit_temp parameter in celsius
     """
-    pass  # remove pass statement and implement me
+    return round((fahrenheit_temp-32)*5/9,2)
 
 
 def convert_to_fahrenheit(celsius_temp: float) -> int:
@@ -18,17 +19,28 @@ def convert_to_fahrenheit(celsius_temp: float) -> int:
     :param celsius_temp: A float representing a temperature in celsius
     :return:  A float representing the corresponding value of the celsius_temp parameter in fahrenheit
     """
+    return round((celsius_temp*9/5))+32
     pass  # remove pass statement and implement me
 
 
 def temperature_tuple(temperatures: Iterable, input_unit_of_measurement: str) -> Tuple[Tuple[float, float]]:
     """
     Given a tuple or a list of temperatures, this function returns a tuple of tuples.
-    Each tuple contains two values. The first is the value of the temperatures parameter. The second is the the value of
-    the first converted to the unit of measurement specified in the input_unit_of_measurement parameter.
+    Each tuple contains two values. The first is the value of the temperatures parameter. The second is the the value of the first converted to the unit of measurement specified in the input_unit_of_measurement parameter.
 
     :param temperatures: An iterable containing temperatures
     :param input_unit_of_measurement: The unit a measure to use to convert the values in the temperatures parameter
     :return: A tuple of tuples
     """
-    pass  # remove pass statement and implement me
+    tup=tuple()
+    result = []
+    if(input_unit_of_measurement=='c'):
+        for value in temperatures:
+            tup+=((float(value),convert_to_fahrenheit(value)),)
+    if(input_unit_of_measurement=='f'):
+        for value in temperatures:
+            tup+=((float(value),convert_to_celsius(value)),)
+
+
+    return tup
+
--- tuple_utils.py
+++ tuple_utils.py
@@ -14,7 +14,8 @@ def tic_tac_toe_finish(board: TicTacToeBoard, pos_y: int, pos_x: int, symbol: st
     :param symbol: The symbol that should be placed in the column (X, or O)
     :return: None
     """
-    pass  # remove pass statement and implement me
+    board[pos_y][pos_x] = symbol
+
 
 
 def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
@@ -25,7 +26,8 @@ def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
     :param instance: An item in the collection parameter
     :return: An integer.
     """
-    pass  # remove pass statement and implement me
+    counter=collection.count(instance)
+    return counter 
 
 
 def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
@@ -38,6 +40,9 @@ def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
     :param entries: A list or tuple
     :return: None
     """
+    for index,entry in zip(indexes,entries):
+        print(f"Index: {index:<10} Entry: {entry}")
+    #print((indexes,entries))
     pass  # remove pass statement and implement me
 
 
@@ -50,5 +55,10 @@ def print_items_with_index(items: Iterable):
     :param items: A tuple or a list
     :return: None
     """
-    pass  # remove pass statement and implement me
+    """     counter=1
+    for item in items:
+        print(f"{counter}: {item}")
+        counter=counter+1 """
 
+    for i,item in enumerate(items,start=1):
+                print(f"{i}: {item}")
